326505fb5a929b61e71487a0357f9e3aa495f6d1
wangxuanju <wangxuanju@users.noreply.github.com>
2019-07-16 11:40:46 +0800

    Create StraightHeader.java

---
+++ spring-demo4/StraightHeader.java
@@ -0,0 +1,37 @@
+package com.netease.course;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("header")
+public class StraightHeader implements Header {
+	@Value("${color}")
+	private String color;
+	@Value("${size}")
+	private int size;
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("init the header");
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("destroy the header");
+	}
+	
+
+	
+
+	public void doWork() {
+		System.out.println("Do work with straight header");
+
+	}
+
+	public String getInfo() {
+		return "StraightHeader:color" + color+",size="+ size ;
+	}
+
+}
