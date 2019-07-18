a369b13c688c7aa9e62cd76d83715bb6d2d4a850
wangxuanju <wangxuanju@users.noreply.github.com>
2019-07-18 11:10:08 +0800

    Create Account.java

---
+++ Spring事务管理/Account.java
@@ -0,0 +1,20 @@
+package com.netease.course;
+
+public class Account {
+	private String user;
+	private double balance;
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+
+}
